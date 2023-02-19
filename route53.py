b051309c6ef23bb1ca8580c0aaa6af91de2e146d
tharun.k <csetarun2@gmail.com>
2023-02-19 21:27:35 +0530

    Create route53.py

---
+++ route53.py
@@ -0,0 +1,23 @@
+from jinja2 import Environment, FileSystemLoader
+import sys
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("dns_record_temp.json")
+
+zone_id=sys.argv[1]
+record_name=sys.argv[2]
+record_type=sys.argv[3]
+ttl_time=sys.argv[4]
+record_values=sys.argv[5]
+
+
+content = template.render(
+        record_name=record_name,
+        record_type=record_type,
+        ttl_time=ttl_time,
+        record_values=record_values
+    )
+print(content)
+filename="recordset.json"
+with open(filename, mode="w", encoding="utf-8") as message:
+    message.write(content)
