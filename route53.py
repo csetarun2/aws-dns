e6a6f0c6bdfdc338243b4a33eff7c2e8b7e0d105
tharun.k <csetarun2@gmail.com>
2023-02-19 21:44:52 +0530

    Update route53.py

--- route53.py
+++ route53.py
@@ -9,9 +9,10 @@ record_name=sys.argv[2]
 record_type=sys.argv[3]
 ttl_time=sys.argv[4]
 record_values=sys.argv[5]
-
+record_action=sys.argv[6]
 
 content = template.render(
+        record_action=record_action,
         record_name=record_name,
         record_type=record_type,
         ttl_time=ttl_time,
@@ -21,3 +22,7 @@ print(content)
 filename="recordset.json"
 with open(filename, mode="w", encoding="utf-8") as message:
     message.write(content)
+
+
+
+
